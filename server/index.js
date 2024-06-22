3e6acfa23b3f2068235d9612a6e9798fb123383e
arnob-bro <abdulmohaimenkhanarnob@gmail.com>
2024-06-22 06:29:25 +0600

    mongoDB has been connected to the project

--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 dotenv.config();
 
+//connecting mongodb
+const connectDb = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log("server is connected to db");
+  } catch (err) {
+    console.log("server is nottttttt connected to db");
+  }
+};
+
+connectDb();
+
 app.get("/", (req, res) => {
-  res.send("API is running1234");
+  res.send("API is running123");
 });
 
-app.listen(
-  process.env.PORT,
-  console.log(`Server is running at http://localhost:${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server is running at http://localhost:${PORT}`));
