4ae75fb589e5b7d4ea55a530ffd89558cdd7020e
arnob-bro <abdulmohaimenkhanarnob@gmail.com>
2024-07-12 01:21:01 +0600

    try 8

--- server/index.js
+++ server/index.js
@@ -9,17 +9,20 @@ const app = express();
 dotenv.config();
 
 // Middleware to set CORS headers
-/*app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://auto-mate-mern-app.vercel.app"
+  );
   // Add other headers as needed
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  /*res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");*/
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
-});*/
+});
 
 app.use(
   cors({
