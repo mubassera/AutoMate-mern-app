18429b4dbbe5ba7fc27cd69c3b2eaf0ba9c98901
mubassera <mubasseramahdi@gmail.com>
2024-09-12 02:16:58 +0600

    admin parts frontend complete

--- client/src/components/Admin/AdminSidebar/AdminSidebar.jsx
+++ client/src/components/Admin/AdminSidebar/AdminSidebar.jsx
@@ -1,4 +1,3 @@
-// src/components/AdminSidebar/AdminSidebar.jsx
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './AdminSidebar.css';
@@ -7,34 +6,33 @@ export const AdminSidebar = () => {
   return (
     <div className="admin-sidebar">
 
-
       <div className='sideBarLogo'>
-        <h2 className='logo'>LOGO</h2>
-        <h1 className='name'>Automate</h1>
+        {/* Wrap "AutoMate" with NavLink to make it a clickable link */}
+        <NavLink to="/home" className="logo-link">
+          <h1 className='name'>AutoMate</h1>
+        </NavLink>
       </div>
 
-    <div className="underline"></div>
+      <div className="underline"></div>
 
       <ul>
         <li>
           <NavLink to="/admin-page" className={({ isActive }) => (isActive ? 'active' : '')}>Dashboard</NavLink>
         </li>
-        <div className="underline" ></div>
+        <div className="underline"></div>
         <li>
           <NavLink to="/users" className={({ isActive }) => (isActive ? 'active' : '')}>All Users</NavLink>
         </li>
-         <div className="underline"></div>
-         <li>
-          <NavLink to="/items" className={({ isActive }) => (isActive ? 'active' : '')}>Parts</NavLink>
-        </li>
-         <div className="underline"></div>
+        <div className="underline"></div>
         <li>
-          <NavLink to="/Settings" className={({ isActive }) => (isActive ? 'active' : '')}>Settings</NavLink>
+          <NavLink to="/adminParts" className={({ isActive }) => (isActive ? 'active' : '')}>Parts</NavLink>
         </li>
-         <div className="underline"></div>
+        <div className="underline"></div>
+        <li>
+          <NavLink to="/settings" className={({ isActive }) => (isActive ? 'active' : '')}>Settings</NavLink>
+        </li>
+        <div className="underline"></div>
       </ul>
-
-
 
     </div>
   );
